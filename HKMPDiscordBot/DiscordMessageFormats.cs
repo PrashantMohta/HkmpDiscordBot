ee3eb49a725cfd55d4bbea847daf2276b8621af1
Prashant Mohta <mohtaprashantrocks@gmail.com>
2023-07-20 23:13:39 +0530

    handle errors on the discord side too

--- HKMPDiscordBot/DiscordMessageFormats.cs
+++ HKMPDiscordBot/DiscordMessageFormats.cs
@@ -28,12 +28,18 @@ namespace HKMPDiscordBot
                 .WithColor(new Color(50, 0, 0))
                 .WithAuthor($"{bot.BotName} Says")
                 .WithDescription(error);
-            if (bot.adminChannel == null)
+            try
             {
-                bot.adminChannel = await _client.GetChannelAsync(bot.AdminChannelId) as IMessageChannel;
+                if (bot.adminChannel == null)
+                {
+                    bot.adminChannel = await _client.GetChannelAsync(bot.AdminChannelId) as IMessageChannel;
+                }
+                await bot.adminChannel!.SendMessageAsync(null, false, embed.WithCurrentTimestamp().Build());
             }
-            await bot.adminChannel!.SendMessageAsync(null, false, embed.WithCurrentTimestamp().Build());
-
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
         public async void SendResponseMessageToAdmin(BotInstance bot, string message)
         {
@@ -42,27 +48,38 @@ namespace HKMPDiscordBot
                 .WithAuthor($"{bot.BotName} Says")
                 .WithDescription(message);
 
-            if (bot.adminChannel == null)
+            try
             {
-                bot.adminChannel = await _client.GetChannelAsync(bot.AdminChannelId) as IMessageChannel;
+                if (bot.adminChannel == null)
+                {
+                    bot.adminChannel = await _client.GetChannelAsync(bot.AdminChannelId) as IMessageChannel;
+                }
+                await bot.adminChannel!.SendMessageAsync(null, false, embed.WithCurrentTimestamp().Build());
             }
-            await bot.adminChannel!.SendMessageAsync(null, false, embed.WithCurrentTimestamp().Build());
+            catch (Exception ex)
+            {
+                SendErrorMessageToAdmin(bot, ex.ToString());
+            }
 
         }
 
         public async void SendResponseMessageToUsers(BotInstance bot, WebhookData w)
         {
             if (bot.IsMuted) { return; }
-            if (bot.mainChannel == null)
-            {
-                bot.mainChannel = await _client.GetChannelAsync(bot.ChannelId) as IMessageChannel;
-            }
             var embed = new EmbedBuilder()
                 .WithColor(new Color(0, 0, 0))
                 .WithAuthor($"{w.UserName} Says")
                 .WithDescription(w.Message)
                 .WithFooter($"From {w.CurrentScene}");
-            await bot.mainChannel!.SendMessageAsync(null, false, embed.Build());
+            try {
+                if (bot.mainChannel == null)
+                {
+                    bot.mainChannel = await _client.GetChannelAsync(bot.ChannelId) as IMessageChannel;
+                }
+                await bot.mainChannel!.SendMessageAsync(null, false, embed.Build());
+            } catch (Exception ex) {
+                SendErrorMessageToAdmin(bot, ex.ToString());
+            }
         }
 
     }
